2ebf2dff4ea821e0871bb764f824bbf08c56e8c6
adoptermedia <86446451+adoptermedia@users.noreply.github.com>
2025-08-01 21:08:13 -0700

    fix: draw halfpipe without canvas translation

--- scaffolding/demo/phasers-revenge/src/main.js
+++ scaffolding/demo/phasers-revenge/src/main.js
@@ -1,42 +1,20 @@
-import { Game } from "phaser";
-import { Preloader } from "./preloader";
-import { GameOverScene } from "./scenes/GameOverScene";
-import { HudScene } from "./scenes/HudScene";
-import { MainScene } from "./scenes/MainScene";
-import { MenuScene } from "./scenes/MenuScene";
-import { SplashScene } from "./scenes/SplashScene";
+import Phaser from 'phaser';
+import Menu from './scenes/Menu';
+import Game from './scenes/Game';
 
-// More information about config: https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
     type: Phaser.AUTO,
-    parent: "phaser-container",
-    width: 960,
-    height: 540,
-    backgroundColor: "#1c172e",
-    pixelArt: true,
-    roundPixel: false,
-    max: {
-        width: 800,
-        height: 600,
-    },
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
+    width: 800,
+    height: 600,
+    parent: 'phaser-container',
     physics: {
-        default: "arcade",
+        default: 'arcade',
         arcade: {
-            gravity: { y: 0 }
+            gravity: { y: 1000 },
+            debug: false
         }
     },
-    scene: [
-        Preloader,
-        SplashScene,
-        MainScene,
-        MenuScene,
-        HudScene,
-        GameOverScene
-    ]
+    scene: [Menu, Game]
 };
 
-new Game(config);
+export default new Phaser.Game(config);
